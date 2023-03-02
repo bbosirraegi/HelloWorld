4abfde7bb2b44246fc2c76a0f4602a0bb676868b
yungyo <98565092+yungyo@users.noreply.github.com>
2023-03-03 00:26:56 +0900

    [UPDATE] SKELETON

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IndexRouter from './route';
+import { MainLayout } from './components';
+
+const App = () => {
+  return (
+    <IndexRouter />
+    // <MainLayout />
+  );
+};
+
+export default App;
