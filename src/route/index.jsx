360f6df1f7a7748c93619dc6c6049effd82ddb4e
yungyo <dbsry129@naver.com>
2023-04-04 18:32:54 +0900

    eshacke

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "components/layout/MainLayout";
-import { Topic, Main, Posts, SignIn, Mypage, TopicDetail } from "route/pages";
+import { Topic, Main, Posts, SignIn, Mypage, TopicDetail, MainDetail } from "route/pages";
 import ModalTest from "components/Modal";
 import AdminPage from "./pages/AdminPage";
 import { useTopicContext } from "App";
-import MainDetail from "./pages/Main/MainDetail";
+//import MainDetail from "./pages/Main/MainDetail";
 
 const IndexRouter = ({ isLoggedIn, userObj, refreshUser }) => {
   const topic = useTopicContext();
@@ -25,10 +25,9 @@ const IndexRouter = ({ isLoggedIn, userObj, refreshUser }) => {
 
         <Route path="/">
           <Route index element={<Main userObj={userObj} />} />
-          {/* <Route path=":uuid" element={<MainDetail userObj={userObj} />}/> */}
+          <Route path=":uuid" element={<MainDetail userObj={userObj} />}/>
         </Route>
-        
-        <Route path=":uuid" element={<MainDetail />} />
+
         <Route path="/topic">
           <Route index element={<Topic userObj={userObj} />} />
           <Route
